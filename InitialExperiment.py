fb345813b0edf328b4516a33d636aa2e8a0a6f50
IanJoffe <ianjoffe@uchicago.edu>
2024-06-25 11:59:04 -0500

    same as last commit, but with num_workers=0

--- InitialExperiment.py
+++ InitialExperiment.py
@@ -37,6 +37,46 @@ class two_layer_relu_network(nn.Module):
         a1 = self.relu(z1)
         z2 = self.fc2(a1)
         return z2
+    
+
+def train_ground_truth_model(d):
+    """
+    Train a model that may be used as the ground truth function in the experiment
+    """
+    def sin_sum(x):
+        return torch.sin(torch.sum(x, dim=1).unsqueeze(1))
+
+    torch.manual_seed(2024)
+    input_data = torch.normal(mean=0, std=1, size=(100, d)).to(device)
+    input_data = input_data / torch.norm(input_data, dim=0)
+    output_data = sin_sum(input_data).to(device)
+    dataset = simpleDataset(input_data, output_data)
+    dataloader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
+
+    ground_truth_model = two_layer_relu_network(d, 1, 5000).to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(ground_truth_model.parameters(), lr=3e-4)
+
+    print("Training ground truth model")
+    for epoch in np.arange(5000):       # NN empirically converges by this time
+        all_loss = torch.tensor([]).to(device)
+        for data, labels in dataloader:
+
+            optimizer.zero_grad()
+            model_output = ground_truth_model(data)
+            label_noise = torch.normal(mean=0, std=0.05, size=(1,)).to(device)
+            loss = criterion(model_output, labels + label_noise)
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+            all_loss = torch.cat((all_loss, loss.unsqueeze(0)))
+
+        if epoch % int(500) == 0:
+            print("Loss: ", torch.mean(all_loss))
+    print("Completed training ground truth model")
+
+    return train_ground_truth_model
+
   
 def train_model(width, d,
                 device, dataloader, input_data, output_data, valid_input, valid_output,
@@ -76,11 +116,11 @@ def train_model(width, d,
             # take measurments
             if epoch % int(epochs/num_measurements) == 0:
                 measured_epochs.append(epoch)
-                train_loss.append(criterion(model(input_data), output_data).item())
+                train_loss.append(criterion(model(input_data.to(device)), output_data.to(device)).item())
                 valid_loss.append(criterion(model(valid_input), valid_output).item())
-                train_accuracy.append(torch.isclose(model(input_data), output_data, atol=convergence_req).float().mean().item())
+                train_accuracy.append(torch.isclose(model(input_data.to(device)), output_data.to(device), atol=convergence_req).float().mean().item())
                 valid_accuracy.append(torch.isclose(model(valid_input), valid_output, atol=convergence_req).float().mean().item())
-                sharpness.append(get_sharpness(input_data, model))    # causes warning and won't run on cuda only for first epoch
+                sharpness.append(get_sharpness(input_data.to(device), model))    # causes warning and won't run on cuda only for first epoch
                 with model_checkpoints_lock:
                     model_checkpoints_output[int(width)][epoch] = copy.deepcopy(model)
                 
@@ -88,6 +128,8 @@ def train_model(width, d,
             for data, labels in dataloader:
 
                 # take training step
+                data = data.to(device)
+                labels = labels.to(device)
                 model_output = model(data)
                 label_noise = label_noise_dist(**label_noise_dist_args, size=(dataloader.batch_sampler.batch_size,)).unsqueeze(dim=1).to(device)
                 if use_sam:
@@ -126,44 +168,6 @@ def train_model(width, d,
     with experiment_results_lock:
         experiment_results_output[int(width)] = {"converged":converged, "epochs":measured_epochs, "train_loss":train_loss, "valid_loss":valid_loss, "train_accuracy":train_accuracy, "valid_accuracy":valid_accuracy, "sharpness": sharpness}
     return
-
-def train_ground_truth_model(d):
-    """
-    Train a model that may be used as the ground truth function in the experiment
-    """
-    def sin_sum(x):
-        return torch.sin(torch.sum(x, dim=1).unsqueeze(1))
-
-    torch.manual_seed(2024)
-    input_data = torch.normal(mean=0, std=1, size=(100, d)).to(device)
-    input_data = input_data / torch.norm(input_data, dim=0)
-    output_data = sin_sum(input_data).to(device)
-    dataset = simpleDataset(input_data, output_data)
-    dataloader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
-
-    ground_truth_model = two_layer_relu_network(d, 1, 5000).to(device)
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.SGD(ground_truth_model.parameters(), lr=3e-4)
-
-    print("Training ground truth model")
-    for epoch in np.arange(5000):       # NN empirically converges by this time
-        all_loss = torch.tensor([]).to(device)
-        for data, labels in dataloader:
-
-            optimizer.zero_grad()
-            model_output = ground_truth_model(data)
-            label_noise = torch.normal(mean=0, std=0.05, size=(1,)).to(device)
-            loss = criterion(model_output, labels + label_noise)
-            loss.backward(retain_graph=True)
-            optimizer.step()
-
-            all_loss = torch.cat((all_loss, loss.unsqueeze(0)))
-
-        if epoch % int(500) == 0:
-            print("Loss: ", torch.mean(all_loss))
-    print("Completed training ground truth model")
-
-    return train_ground_truth_model
   
   
 def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
@@ -198,10 +202,10 @@ def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
 
     # generate data
     torch.manual_seed(random_seed)
-    input_data = input_dist(**input_dist_args, size=(n, d)).to(device)
+    input_data = input_dist(**input_dist_args, size=(n, d))
     if normalize_input:
         input_data = input_data / (torch.norm(input_data, dim=1).unsqueeze(1))
-    output_data = true_function(input_data).to(device)
+    output_data = true_function(input_data)
 
     valid_input = input_dist(**input_dist_args, size=(n_valid, d)).to(device)
     if normalize_input:
@@ -209,7 +213,7 @@ def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
     valid_output = true_function(valid_input).to(device)
 
     dataset = simpleDataset(input_data, output_data)
-    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle_data)
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle_data, num_workers=0)
 
     # manage parallelism
     processes = []
@@ -256,7 +260,7 @@ if __name__ == "__main__":
     mp.set_start_method("spawn")    # may want to set force=True if issues
 
     # ground_truth_model = train_ground_truth_model(d=30)
-    trained_models, results = run_width_experiment(n=300, d=30, m=np.array([10,15,20]), shuffle_data=True, true_function=xor, convergence_req=1e-2, lr=24.5, batch_size=300, label_noise_dist=torch_binary_label_noise, label_noise_dist_args={"sd":0.005}, max_epochs=2e6)
+    trained_models, results = run_width_experiment(n=300, d=30, m=np.arange(10,19,2), shuffle_data=True, true_function=xor, convergence_req=1e-2, lr=12.5, batch_size=300, label_noise_dist=torch_binary_label_noise, label_noise_dist_args={"sd":0.01}, max_epochs=3e6)
     
     for m in trained_models.keys():
         (Path(args.results_file).parent / ("checkpoints_" + str(m))).mkdir(parents=True, exist_ok=True)
