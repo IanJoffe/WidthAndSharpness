777ebd2487b7b6f913c1eb2bc65910a0e0546b65
IanJoffe <ianjoffe@uchicago.edu>
2024-06-26 22:42:34 -0500

    Allow visualizations to merge runs, plus debugging multithreading

--- InitialExperiment.py
+++ InitialExperiment.py
@@ -37,46 +37,6 @@ class two_layer_relu_network(nn.Module):
         a1 = self.relu(z1)
         z2 = self.fc2(a1)
         return z2
-    
-
-def train_ground_truth_model(d):
-    """
-    Train a model that may be used as the ground truth function in the experiment
-    """
-    def sin_sum(x):
-        return torch.sin(torch.sum(x, dim=1).unsqueeze(1))
-
-    torch.manual_seed(2024)
-    input_data = torch.normal(mean=0, std=1, size=(100, d)).to(device)
-    input_data = input_data / torch.norm(input_data, dim=0)
-    output_data = sin_sum(input_data).to(device)
-    dataset = simpleDataset(input_data, output_data)
-    dataloader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
-
-    ground_truth_model = two_layer_relu_network(d, 1, 5000).to(device)
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.SGD(ground_truth_model.parameters(), lr=3e-4)
-
-    print("Training ground truth model")
-    for epoch in np.arange(5000):       # NN empirically converges by this time
-        all_loss = torch.tensor([]).to(device)
-        for data, labels in dataloader:
-
-            optimizer.zero_grad()
-            model_output = ground_truth_model(data)
-            label_noise = torch.normal(mean=0, std=0.05, size=(1,)).to(device)
-            loss = criterion(model_output, labels + label_noise)
-            loss.backward(retain_graph=True)
-            optimizer.step()
-
-            all_loss = torch.cat((all_loss, loss.unsqueeze(0)))
-
-        if epoch % int(500) == 0:
-            print("Loss: ", torch.mean(all_loss))
-    print("Completed training ground truth model")
-
-    return train_ground_truth_model
-
   
 def train_model(width, d,
                 device, dataloader, input_data, output_data, valid_input, valid_output,
@@ -96,7 +56,7 @@ def train_model(width, d,
     epochs = max_epochs
 
     with model_checkpoints_lock:
-        model_checkpoints_output[int(width)] = {}
+        model_checkpoints_output[int(width)] = dict()
     measured_epochs = []
     train_loss = []
     valid_loss = []
@@ -116,20 +76,20 @@ def train_model(width, d,
             # take measurments
             if epoch % int(epochs/num_measurements) == 0:
                 measured_epochs.append(epoch)
-                train_loss.append(criterion(model(input_data.to(device)), output_data.to(device)).item())
+                train_loss.append(criterion(model(input_data), output_data).item())
                 valid_loss.append(criterion(model(valid_input), valid_output).item())
-                train_accuracy.append(torch.isclose(model(input_data.to(device)), output_data.to(device), atol=convergence_req).float().mean().item())
+                train_accuracy.append(torch.isclose(model(input_data), output_data, atol=convergence_req).float().mean().item())
                 valid_accuracy.append(torch.isclose(model(valid_input), valid_output, atol=convergence_req).float().mean().item())
-                sharpness.append(get_sharpness(input_data.to(device), model))    # causes warning and won't run on cuda only for first epoch
+                sharpness.append(get_sharpness(input_data, model))    # causes warning and won't run on cuda only for first epoch
                 with model_checkpoints_lock:
-                    model_checkpoints_output[int(width)][epoch] = copy.deepcopy(model)
+                    model_checkpoints_output_width = model_checkpoints_output[int(width)]
+                    model_checkpoints_output_width[epoch] = copy.deepcopy(model).to(torch.device('cpu'))
+                    model_checkpoints_output[int(width)] = model_checkpoints_output_width
                 
             all_loss = torch.tensor([]).to(device)
             for data, labels in dataloader:
 
                 # take training step
-                data = data.to(device)
-                labels = labels.to(device)
                 model_output = model(data)
                 label_noise = label_noise_dist(**label_noise_dist_args, size=(dataloader.batch_sampler.batch_size,)).unsqueeze(dim=1).to(device)
                 if use_sam:
@@ -168,6 +128,44 @@ def train_model(width, d,
     with experiment_results_lock:
         experiment_results_output[int(width)] = {"converged":converged, "epochs":measured_epochs, "train_loss":train_loss, "valid_loss":valid_loss, "train_accuracy":train_accuracy, "valid_accuracy":valid_accuracy, "sharpness": sharpness}
     return
+
+def train_ground_truth_model(d):
+    """
+    Train a model that may be used as the ground truth function in the experiment
+    """
+    def sin_sum(x):
+        return torch.sin(torch.sum(x, dim=1).unsqueeze(1))
+
+    torch.manual_seed(2024)
+    input_data = torch.normal(mean=0, std=1, size=(100, d)).to(device)
+    input_data = input_data / torch.norm(input_data, dim=0)
+    output_data = sin_sum(input_data).to(device)
+    dataset = simpleDataset(input_data, output_data)
+    dataloader = DataLoader(dataset=dataset, batch_size=1, shuffle=False)
+
+    ground_truth_model = two_layer_relu_network(d, 1, 5000).to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.SGD(ground_truth_model.parameters(), lr=3e-4)
+
+    print("Training ground truth model")
+    for epoch in np.arange(5000):       # NN empirically converges by this time
+        all_loss = torch.tensor([]).to(device)
+        for data, labels in dataloader:
+
+            optimizer.zero_grad()
+            model_output = ground_truth_model(data)
+            label_noise = torch.normal(mean=0, std=0.05, size=(1,)).to(device)
+            loss = criterion(model_output, labels + label_noise)
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+            all_loss = torch.cat((all_loss, loss.unsqueeze(0)))
+
+        if epoch % int(500) == 0:
+            print("Loss: ", torch.mean(all_loss))
+    print("Completed training ground truth model")
+
+    return train_ground_truth_model
   
   
 def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
@@ -200,12 +198,14 @@ def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
     random_seed: int
     """
 
+    model_parameters = {"n": n, "d": d, "lr":lr, "label_noise_sd":(label_noise_dist_args["sd"] if "sd" in label_noise_dist_args else "NA")}
+
     # generate data
     torch.manual_seed(random_seed)
-    input_data = input_dist(**input_dist_args, size=(n, d))
+    input_data = input_dist(**input_dist_args, size=(n, d)).to(device)
     if normalize_input:
         input_data = input_data / (torch.norm(input_data, dim=1).unsqueeze(1))
-    output_data = true_function(input_data)
+    output_data = true_function(input_data).to(device)
 
     valid_input = input_dist(**input_dist_args, size=(n_valid, d)).to(device)
     if normalize_input:
@@ -213,7 +213,7 @@ def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
     valid_output = true_function(valid_input).to(device)
 
     dataset = simpleDataset(input_data, output_data)
-    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle_data, num_workers=0)
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle_data)
 
     # manage parallelism
     processes = []
@@ -237,7 +237,7 @@ def run_width_experiment(n=100, n_valid=1000, d=10, m=list(range(10, 100, 5)),
         p.join()
 
     print("Completed training experimental models")
-    return model_checkpoints, experiment_results
+    return model_parameters, model_checkpoints, experiment_results
 
 def xor(x):
     return (x[:, 1] * x[:, 2]).unsqueeze(1)
@@ -255,16 +255,18 @@ if __name__ == "__main__":
     Path(args.results_file).parent.mkdir(parents=True, exist_ok=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.cuda.init()
+    torch.cuda.init() 
     print("Connected to", str(device))
     mp.set_start_method("spawn")    # may want to set force=True if issues
 
     # ground_truth_model = train_ground_truth_model(d=30)
-    trained_models, results = run_width_experiment(n=300, d=30, m=np.arange(10,19,2), shuffle_data=True, true_function=xor, convergence_req=1e-2, lr=12.5, batch_size=300, label_noise_dist=torch_binary_label_noise, label_noise_dist_args={"sd":0.01}, max_epochs=3e6)
+    model_parameters, trained_models, results = run_width_experiment(n=300, d=30, m=np.array([10,20]), shuffle_data=True, true_function=xor, convergence_req=1e-2, lr=12.5, batch_size=300, label_noise_dist=torch_binary_label_noise, label_noise_dist_args={"sd":0.01}, max_epochs=400)
     
     for m in trained_models.keys():
-        (Path(args.results_file).parent / ("checkpoints_" + str(m))).mkdir(parents=True, exist_ok=True)
+        (Path(args.results_file).parent / ("checkpoints_" + str(m))).mkdir()
         for epoch in trained_models[m].keys():
             torch.save(trained_models[m][epoch], Path(args.results_file).parent / ("checkpoints_" + str(m)) / ("model_" + str(m) + "_epoch_" + str(epoch) + ".pth"))
     with open(args.results_file, 'w') as f:
         json.dump(dict(results), f)
+    with open(Path(args.results_file).parent / "model_params.json", 'w') as f:
+        json.dump(model_parameters, f)
--- VizInitialExperiment.py
+++ VizInitialExperiment.py
@@ -6,7 +6,7 @@ import torch
 import matplotlib.pyplot as plt
 import json
 
-def graph_feature(results, feature, widths=None, when="last", moving_average=False):
+def graph_feature(results, feature, widths=None, when="last", moving_average=False, caption=None):
     if widths == None:
         widths = sorted(list(results.keys()), key=int)   # allows you to only plot some widths
     if when == "last":
@@ -26,9 +26,11 @@ def graph_feature(results, feature, widths=None, when="last", moving_average=Fal
     ax.scatter(widths, y)
     ax.set_xlabel("Width")
     ax.set_ylabel(feature + " after Completed Training")
+    if caption:
+        fig.text(0.5, -0.03, caption, ha='center', va='center')
     return fig
 
-def graph_curve(results, feature, width, apply_log=True, moving_average=False):
+def graph_curve(results, feature, width, apply_log=True, moving_average=False, caption=None):
     # feature MUST be "loss_curve", or some other similarly formatted array
     vals = torch.tensor(results[width][feature])
     if moving_average:
@@ -45,9 +47,11 @@ def graph_curve(results, feature, width, apply_log=True, moving_average=False):
     ax.set_ylabel(feature)
     ax.set_title(feature + " curve for m = " + str(width) + " over training")
     ax.set_xlabel("Epoch")
+    if caption:
+        fig.text(0.5, -0.03, caption, ha='center', va='center')
     return fig
 
-def plot_weights_progression(checkpoints_list, results, width, min_dim=None, max_dim=None, min_neuron=None, max_neuron=None):
+def plot_weights_progression(checkpoints_list, results, width, min_dim=None, max_dim=None, min_neuron=None, max_neuron=None, caption=None):
     fc1_weights_list = [model.fc1.weight.data.detach().cpu().numpy() for model in checkpoints_list]
     fc1_weights_list_to_plot = np.array([weights[min_neuron:max_neuron,min_dim:max_dim].flatten() for weights in fc1_weights_list]).T
     fig, ax = plt.subplots()
@@ -56,9 +60,11 @@ def plot_weights_progression(checkpoints_list, results, width, min_dim=None, max
     ax.set_ylabel("Weight")
     ax.set_xlabel("Epoch")
     ax.set_title("Progression of FC1 Weights Over Training, m=" + str(width))
+    if caption:
+        fig.text(0.5, -0.03, caption, ha='center', va='center')
     return fig
 
-def plot_final_weights(saved_model, width, min_dim=1, max_dim=3):
+def plot_final_weights(saved_model, width, min_dim=1, max_dim=3, caption=None):
     assert max_dim - min_dim == 2, "Cannot plot weights for d > 2"
     weights_fc1 = saved_model.fc1.weight.data.detach().cpu().numpy()
     informative_weights_fc1 = weights_fc1[:,min_dim:max_dim]
@@ -67,13 +73,17 @@ def plot_final_weights(saved_model, width, min_dim=1, max_dim=3):
     ax.set_xlim(1.2 * min(min(informative_weights_fc1[:,0]), min(informative_weights_fc1[:,1])), 1.2 * max(max(informative_weights_fc1[:,0]), max(informative_weights_fc1[:,1])))
     ax.set_ylim(1.2 * min(min(informative_weights_fc1[:,0]), min(informative_weights_fc1[:,1])), 1.2 * max(max(informative_weights_fc1[:,0]), max(informative_weights_fc1[:,1])))
     ax.set_title("First Layer Weights for Informative Dimensions, m = " + str(width))
+    if caption:
+        fig.text(0.5, -0.03, caption, ha='center', va='center')
     return fig
 
-def hist_weights(saved_model, width, min_dim=3, max_dim=None):
+def hist_weights(saved_model, width, min_dim=3, max_dim=None, caption=None):
     weights_fc1 = saved_model.fc1.weight.data.detach().cpu().numpy()
     fig, ax = plt.subplots()
     ax.hist(weights_fc1[:,min_dim:max_dim].flatten(), bins=100)
     ax.set_title("First Layer Weights for Spurious Dimensions, m = " + str(width))
+    if caption:
+        fig.text(0.5, -0.03, caption, ha='center', va='center')
     return fig
 
 
@@ -81,49 +91,72 @@ def hist_weights(saved_model, width, min_dim=3, max_dim=None):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("results_file", type=str, help="The path to the json results file saved by InitialExperiment.py")
+    parser.add_argument("--only", type=str, help="Only generate the named visualization. Can be one of or a comma-seperated list of 'feature', 'curve', 'progression', or 'weights'")
     args = parser.parse_args()
 
-    with open(args.results_file, 'r') as f:
-        results = json.load(f)
+    if args.only:
+        only = args.only.split(",")
+    else:
+        only=None
+    all_results_files = args.results_file.split(",")
+    results_file = all_results_files[0]
+    if len(all_results_files) == 1:
+        with open(all_results_files[0], 'r') as f:
+            results = json.load(f)
+    else:
+        results = {}
+        for results_file in all_results_files:
+            with open(results_file, 'r') as f:
+                current_results_file = json.load(f)
+                if results.keys() & current_results_file.keys():
+                    assert "Tried to merge results from two experiments with the same widths"
+                results = results | current_results_file
 
-    for feature in ["sharpness", "train_loss", "valid_loss"]:
-        figure_tosave = graph_feature(results, feature, when="best", moving_average=False)
-        Path(args.results_file).parent.mkdir(parents=True, exist_ok=True)
-        figure_tosave.savefig(Path(args.results_file).parent /
-                                (feature + "final" + ".png"),
+    with open(Path(results_file).parent / "model_params.json", 'r') as f:
+        caption = str(json.load(f))
+
+    if not only or "feature" in only:
+        for feature in ["sharpness", "train_loss", "valid_loss"]:
+            figure_tosave = graph_feature(results, feature, when="best", moving_average=False, caption=caption)
+            Path(results_file).parent.mkdir(parents=True, exist_ok=True)
+            figure_tosave.savefig(Path(results_file).parent /
+                                    (feature + "final" + ".png"),
+                                    bbox_inches="tight")
+    
+    if not only or "curve" in only:
+        for width in results.keys():
+            for feature in ["sharpness", "train_loss", "valid_loss", "train_accuracy", "valid_accuracy"]:
+                if feature in ["sharpness", "train_loss", "valid_loss"]:
+                    figure_tosave = graph_curve(results, feature, width, moving_average=False)
+                else:
+                    figure_tosave = graph_curve(results, feature, width, apply_log=False, moving_average=False)
+                figure_tosave.savefig(Path(results_file).parent /
+                                        (feature + "_" + str(width) + ".png"),
+                                        bbox_inches="tight")
+            
+    if not only or "progression" in only:
+        checkpoint_subdirs = [d for d in Path(results_file).parent.iterdir() if (d.is_dir() and d.name.startswith("checkpoints"))]
+        for checkpoint_subdir in checkpoint_subdirs:
+            width = checkpoint_subdir.name.split("_")[-1]
+            checkpoints_list_fnames = sorted([ckpt for ckpt in checkpoint_subdir.iterdir()], key=lambda fname: int(fname.stem.split("_")[-1]))
+            checkpoints_list = [torch.load(ckpt) for ckpt in checkpoints_list_fnames]
+            figure_tosave = plot_weights_progression(checkpoints_list, results, width)
+            figure_tosave.savefig(Path(results_file).parent /
+                                ("weights_progression_" + str(width) + ".png"),
                                 bbox_inches="tight")
-        
-    for width in results.keys():
-        for feature in ["sharpness", "train_loss", "valid_loss", "train_accuracy", "valid_accuracy"]:
-            if feature in ["sharpness", "train_loss", "valid_loss"]:
-                figure_tosave = graph_curve(results, feature, width, moving_average=False)
-            else:
-                figure_tosave = graph_curve(results, feature, width, apply_log=False, moving_average=False)
-            figure_tosave.savefig(Path(args.results_file).parent /
-                                    (feature + "_" + str(width) + ".png"),
+
+    if not only or "weights" in only:
+        for width in results.keys():
+            all_checkpoints = [ckpt for ckpt in (Path(results_file).parent / ("checkpoints_" + str(width))).iterdir()]
+            final_checkpoint = max(all_checkpoints, key=lambda fname: int(fname.stem.split("_")[-1]))
+            saved_model = torch.load(final_checkpoint)
+            figure_tosave = hist_weights(saved_model, width)
+            figure_tosave.savefig(Path(results_file).parent /
+                                    ("spurious_weights_" + str(width) + ".png"),
+                                    bbox_inches="tight")
+            figure_tosave = plot_final_weights(saved_model, width)
+            figure_tosave.savefig(Path(results_file).parent /
+                                    ("informative_weights_" + str(width) + ".png"),
                                     bbox_inches="tight")
             
-    checkpoint_subdirs = [d for d in Path(args.results_file).parent.iterdir() if (d.is_dir() and d.name.startswith("checkpoints"))]
-    for checkpoint_subdir in checkpoint_subdirs:
-        width = checkpoint_subdir.name.split("_")[-1]
-        checkpoints_list_fnames = sorted([ckpt for ckpt in checkpoint_subdir.iterdir()], key=lambda fname: int(fname.stem.split("_")[-1]))
-        checkpoints_list = [torch.load(ckpt) for ckpt in checkpoints_list_fnames]
-        figure_tosave = plot_weights_progression(checkpoints_list, results, width)
-        figure_tosave.savefig(Path(args.results_file).parent /
-                            ("weights_progression_" + str(width) + ".png"),
-                            bbox_inches="tight")
-
-"""
-    for width in results.keys():
-        all_checkpoints = [ckpt for ckpt in (Path(args.results_file).parent / ("checkpoints_" + str(width))).iterdir()]
-        final_checkpoint = max(all_checkpoints, key=lambda fname: int(fname.stem.split("_")[-1]))
-        saved_model = torch.load(final_checkpoint)
-        figure_tosave = hist_weights(saved_model, width)
-        figure_tosave.savefig(Path(args.results_file).parent /
-                                  ("spurious_weights_" + str(width) + ".png"),
-                                  bbox_inches="tight")
-        figure_tosave = plot_final_weights(saved_model, width)
-        figure_tosave.savefig(Path(args.results_file).parent /
-                                  ("informative_weights_" + str(width) + ".png"),
-                                  bbox_inches="tight")
-"""
+    print("Visualizations saved to " + Path(results_file).parent)
